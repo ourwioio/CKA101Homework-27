98ae8b739421c499185e3c63435ec16e65332cfd
Gary <ourwioioaa123@gmail.com>
2026-04-09 14:37:23 +0800

    hwExt5

--- src/hwExt/RoleGameTest.java
+++ src/hwExt/RoleGameTest.java
@@ -5,10 +5,12 @@ public class RoleGameTest {
 	public static void main(String[] args) {
 		Hero saber = new SwordMan("亞拉岡", 1, 0);
 		Hero archer = new ArrowMan("勒苟拉斯", 1, 0);
+		Move m = new Move();
+		FastMove fm = new FastMove();
 
-		saber.setMove(new Move());
+		saber.setMove(m);
 		saber.setDefend(new Defend());
-		archer.setMove(new Move());
+		archer.setMove(m);
 		archer.setDefend(new Defend());
 				
 		saber.attack();
@@ -19,7 +21,7 @@ public class RoleGameTest {
 		archer.move();
 		archer.defend();
 		
-		archer.setMove(new FastMove());
+		archer.setMove(fm);
 		archer.move();
 	}
 
