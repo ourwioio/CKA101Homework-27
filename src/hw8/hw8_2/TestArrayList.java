3271712624d6deaec0b346e2db039e414ccab110
Gary <ourwioioaa123@gmail.com>
2026-04-21 22:03:09 +0800

    hw8

---
+++ src/hw8/hw8_2/TestArrayList.java
@@ -0,0 +1,42 @@
+package hw8.hw8_2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+//有重複且由大到小排序
+public class TestArrayList {
+	public static void main(String[] args) {
+		List<Train> train = new ArrayList<Train>();
+		
+		train.add(new Train(202, "普悠瑪", "樹林", "花蓮", 400));
+		train.add(new Train(1254, "區間", "屏東", "基隆", 700));
+		train.add(new Train(118, "自強", "高雄", "台北", 500));
+		train.add(new Train (1288, "區間", "新竹", "基隆", 400));
+		train.add(new Train(122, "自強", "台中", "花蓮", 600));
+		train.add(new Train(1222, "區間", "樹林", "七堵", 300));
+		train.add(new Train(1254, "區間", "屏東", "基隆", 700));
+		
+		//會去看Train實作的Comparable介面排序
+		Collections.sort(train);
+		
+//		Iterator<Train> it = train.iterator();
+//		while(it.hasNext()) {
+//			Train t = it.next();
+//			System.out.println("班次：" + t.getNumber() + " 車種：" + t.getType() + " 出發地：" + t.getStart() +
+//					" 目的地：" + t.getDest() + " 票價：" + t.getPrice());
+//		}	
+		
+//		for(Train t : train) {
+//			System.out.println("班次：" + t.getNumber() + " 車種：" + t.getType() + " 出發地：" + t.getStart() +
+//					" 目的地：" + t.getDest() + " 票價：" + t.getPrice());
+//		}
+		
+		for(int i = 0; i < train.size(); i++) {
+			System.out.println("班次：" + train.get(i).getNumber() + " 車種：" + train.get(i).getType() + " 出發地：" + train.get(i).getStart() +
+					" 目的地：" + train.get(i).getDest() + " 票價：" + train.get(i).getPrice());
+		}
+		
+	}
+}
---
+++ src/hw8/hw8_2/Train.java
@@ -0,0 +1,95 @@
+package hw8.hw8_2;
+
+import java.util.Objects;
+
+public class Train implements Comparable<Train>{
+	private int number;
+	private String type;
+	private String start;
+	private String dest;
+	private double price;
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setStart(String start) {
+		this.start = start;
+	}
+	
+	public String getStart() {
+		return start;
+	}
+	
+	public void setDest(String dest) {
+		this.dest = dest;
+	}
+	
+	public String getDest() {
+		return dest;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public Train(int number, String type, String start, String dest, double price) {
+		this.number = number;
+		this.type = type;
+		this.start = start;
+		this.dest = dest;
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dest, number, price, start, type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Train other = (Train) obj;
+		return Objects.equals(dest, other.dest) && number == other.number
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
+				&& Objects.equals(start, other.start) && Objects.equals(type, other.type);
+	}
+
+	//讓TreeSet有辦法排序
+	@Override
+	public int compareTo(Train tTrain) {
+		if(this.number > tTrain.number) {
+			return -1;
+		}else if (this.number == tTrain.number) {
+			return 0;
+		}else
+			return 1;
+	}
+	
+	
+	
+	
+	
+	
+}
