5f704aeb1e9a51a6891ab43bf0d34f1c578ec513
Gary <ourwioioaa123@gmail.com>
2026-04-25 21:56:04 +0800

    hw9_2.4

--- src/hw9/hw9_2/TestMoney.java
+++ src/hw9/hw9_2/TestMoney.java
@@ -4,19 +4,21 @@ class Wallet {
 	private int balance = 0;
 
 	synchronized public void safeMoney(int money) {
-		if (balance >= 3000) {
+
+		while (balance > 3000) {
 			try {
-				System.out.println("媽媽看到餘額在3000以上，暫停匯款");
-				System.out.println("熊大被媽媽告知帳戶已經有錢");
+				System.out.println("\t\t\t媽媽看到餘額在3000以上，暫停匯款");
 				// 因為熊大只有領10次只有1萬元，媽媽存10次有2萬元，錢沒人領程式進入死結。
 				// 在wait()內加入參數讓媽媽等不到人領錢會醒來做接下去的動作，避免死結。
-				wait(100);
+				wait();
 			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} 
+				System.out.println("\t\t\t熊大發財了,不需要媽媽匯款了");
+				return;
+			}
+			System.out.println("\t\t\t媽媽被熊大要求匯款！");
 		}
 		balance += money;
-		System.out.println("媽媽存了" + money + " 帳戶餘額：" + balance);
+		System.out.println("\t\t\t媽媽存了" + money + " 帳戶餘額：" + balance);
 		notify();
 	}
 
@@ -24,11 +26,11 @@ class Wallet {
 		while (balance < money) {
 			try {
 				System.out.println("熊大到看到帳戶沒錢暫停提款");
-				System.out.println("媽媽被熊大要求匯款！");
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			System.out.println("熊大被媽媽告知帳戶已經有錢");
 		}
 		if (balance <= 2000) {
 			System.out.println("熊大看到餘額在2000以下，要求匯款");
@@ -45,14 +47,17 @@ class Wallet {
 
 class Mother extends Thread {
 	Wallet wt;
+	Son son;
 
 	public Mother(Wallet wt) {
 		this.wt = wt;
 	}
 
 	public void run() {
-		for (int i = 1; i <= 10; i++)
+		for (int i = 1; i <= 10; i++) {
 			wt.safeMoney(2000);
+		}
+
 	}
 }
 
@@ -76,6 +81,16 @@ public class TestMoney {
 		Son son = new Son(wt);
 		mother.start();
 		son.start();
+
+		while (true) {
+			if (!son.isAlive()) {
+				mother.interrupt();
+			}
+			if (!mother.isAlive()) {
+				break;
+			}
+		}
+
 	}
 
 }
